98df0e985ac90708a105e4d3d5143cf0b72afc4d
310-02 <310-02@310-02>
2021-12-08 11:55:40 +0500

    Добавьте файлы проекта.

---
+++ module2pr31s5/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace module2pr31s5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void register(object sender, EventArgs e)
+        {
+
+        }
+        public async void navig(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+    }
+}
---
+++ module2pr31s5/Page2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace module2pr31s5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+        public async void navig2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page3());
+        }
+        public void SignUp(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
